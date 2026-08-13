8e6e43f23790a013df8bf66253ca3e93b93fa066
beinuela <481958370@qq.com>
2026-08-13 23:54:31 +0800

    style: update code block styling for improved visibility and aesthetics

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: 'github-light',
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
     rehypePlugins: [
